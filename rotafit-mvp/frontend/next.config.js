714671b9c42d512632031a6811cef23944a1884b
rotafit <rotafit@unavidasaudable.com>
2025-11-25 09:31:08 -0300

    Add files via upload

--- rotafit-mvp/frontend/next.config.js
+++ rotafit-mvp/frontend/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'rotafit.onrender.com'],
+    unoptimized: true
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://rotafit.onrender.com/api',
   }
 }
 
